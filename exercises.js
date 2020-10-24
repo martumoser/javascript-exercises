80e5f7615ed48fafbdc9c9f21efbb577abed1926
Martina Moser <mmoser.ar@gmail.com>
2020-10-24 18:52:24 -0300

    resolution of item 1 'variables y operadores' of the weekly problem

---
+++ exercises.js
@@ -0,0 +1,16 @@
+/* Variables y Operadores */
+/* Item a */
+ var number1 = 12;
+ var number2 = 20;
+ var sum = number1 + number2;
+ 
+ /* Item b */
+ var string1 = "Hi, my name is ";
+ var string2 = "Martina";
+ var miName = string1 + string2;
+ 
+ /* Item c */
+ var str1 = "Dog";
+ var str2 = "Cat";
+ var petLength = str1.length + str2.length;
+ 
