c33d48d9ada399d8b71ed13975396a19adeb10ce
Martina Moser <mmoser.ar@gmail.com>
2020-10-25 12:19:09 -0300

    resolution of item 2 'Strings' of the weekly problem

--- exercises.js
+++ exercises.js
@@ -14,3 +14,30 @@
  var str2 = "Cat";
  var petLength = str1.length + str2.length;
  
+ /*Strings*/
+ /* Item a */
+ var var1 = "environment";
+ console.log(var1 .toUpperCase());
+
+ /* Item b */
+ var weather = "thunderstorm";
+ var today = (weather.substring(5,0));
+ console.log(today);
+
+ /* Item c */
+ var weather2 = "thunderstorm";
+ var day = (weather2.substring(12,9));
+ console.log(day);
+
+ /* Item d */
+ var org = "orGanIzaTion"; /* I put some letters in uppercase in the middle to check if the toLowerCase works ok*/
+ var orgNew = (org.substring(0,1).toLocaleUpperCase()+ org.substring(1).toLowerCase());
+ console.log(orgNew);
+
+ /* Item e */
+ var name = "my name is Martina";
+ var blank = (name.indexOf(" "));
+ console.log(blank);
+
+ /* Item f */
+ 
