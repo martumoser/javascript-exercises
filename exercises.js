3628bcc7c5a0f119837c3e42462e5a21d6445e9f
Martina Moser <mmoser.ar@gmail.com>
2020-10-25 14:11:02 -0300

    resolution of item 3 'Arrays' of the weekly problem

--- exercises.js
+++ exercises.js
@@ -1,43 +1,59 @@
 /* Variables y Operadores */
-/* Item a */
+ /* Item a */
  var number1 = 12;
  var number2 = 20;
  var sum = number1 + number2;
- 
  /* Item b */
  var string1 = "Hi, my name is ";
  var string2 = "Martina";
- var miName = string1 + string2;
- 
+ var miName = string1 + string2; 
  /* Item c */
  var str1 = "Dog";
  var str2 = "Cat";
  var petLength = str1.length + str2.length;
  
- /*Strings*/
+/*Strings*/
  /* Item a */
  var var1 = "environment";
- console.log(var1 .toUpperCase());
-
+ var var1UpperCase = (var1 .toUpperCase());
  /* Item b */
  var weather = "thunderstorm";
  var today = (weather.substring(5,0));
- console.log(today);
-
  /* Item c */
  var weather2 = "thunderstorm";
  var day = (weather2.substring(12,9));
- console.log(day);
-
  /* Item d */
- var org = "orGanIzaTion"; /* I put some letters in uppercase in the middle to check if the toLowerCase works ok*/
+ var org = "organization"; 
  var orgNew = (org.substring(0,1).toLocaleUpperCase()+ org.substring(1).toLowerCase());
- console.log(orgNew);
-
  /* Item e */
  var name = "my name is Martina";
  var blank = (name.indexOf(" "));
- console.log(blank);
-
  /* Item f */
  
+/* Arrays */
+ /* Item a */
+ var months = new Array ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+ console.log(months[4],months[10]);
+ /* Item b */
+ months.sort();
+ console.log (months);
+ /* Item c */
+ var months = new Array ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+ months.push("last");
+ months.unshift("first");
+ console.log(months);
+ /* Item d */
+ months.pop();
+ months.shift();
+ console.log(months);
+ /* Item e */
+ months.reverse();
+ console.log(months);
+ /* Item f */
+ var months = new Array ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+ elements = (months.join("-"));
+ console.log(elements);
+ /* Item g */
+ var copy = months.slice(4,-1);
+ console.log (copy);
+
